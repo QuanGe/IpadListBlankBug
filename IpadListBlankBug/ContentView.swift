54638a49ad4882073415e47e75c7795972a1ae11
ruquan.zhang <zhang_ru_quan@163.com>
2020-02-09 23:43:07 +0800

    Initial Commit

---
+++ IpadListBlankBug/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  IpadListBlankBug
+//
+//  Created by 张汝泉 on 2020/2/9.
+//  Copyright © 2020 QuanGe. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
